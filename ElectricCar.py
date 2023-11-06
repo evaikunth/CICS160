47bfccf0b80ca9c5c4d63ce37342e2fb99ca8c63
evaikunth <145293900+evaikunth@users.noreply.github.com>
2023-11-05 19:28:59 -0500

    Update ElectricCar.py

--- ElectricCar.py
+++ ElectricCar.py
@@ -1,12 +1,23 @@
 import Car
-class ElectricCar(Car.Car):
-    def __init__(self, make_model = "None assigned", num_doors = 4, max_passengers = 5):
-        super().__init__(make_model, num_doors, max_passengers)
-        
+class ElectricCar(Car.Car): 
 
-    def __str__(self) -> str:
-        return super().__str__() + "\nELECTRIC CAR"
+    def __init__(self,makeAndModel:str = "none assigned", numberOfDoors:int = 4, maximumNumberOfPassengers:int = 5, batterySize = -1):
+        super().__init__(makeAndModel,numberOfDoors,maximumNumberOfPassengers)
+        self.batterySize = batterySize
 
+     #getter methods
+  
+    def getBatterySize(self) -> int:
+        return(self.batterySize)
+    
+    #setters
+
+    def setBatterySize(self,batterySize):
+        self.batterySize = batterySize
+
+    def __str__(self):
+        stringToReturn = " make model: " + self.makeModel + "\n number of doors: " + str(self.numDoors) + "\n max passengers: " + str(self.maxPassengers) + "\n battery size " + str(self.batterySize)
+        return(stringToReturn)
+    
 if __name__ == "__main__":
-    e1 = ElectricCar()
-    print(e1)
+    pass
